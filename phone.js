e5b43178f065cd3c0e91c387531d4dbee9dfc87c
sfelton2k <sfelton2000@gmail.com>
2024-01-10 17:47:14 -0500

    Update index.html, add phone.css, add phone.js

---
+++ phone.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $("#tabs").show();
+    $("#dialer").hide();
+    $("#listContacts").hide();
+    $("#add_contacts").hide();
+});
+
+$("#dialInterface").click(function() {
+    $("#dialer").show();
+    $("#listContacts").hide();
+    $("#add_contacts").hide();
+});
+
+$("#contacts").click(function() {
+    $("#dialer").hide();
+    $("#listContacts").show();
+    $("#add_contacts").hide();
+});
+
+$("#addContact").click(function() {
+    $("#dialer").hide();
+    $("#listContacts").hide();
+    $("#add_contacts").show();
+});
